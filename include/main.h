4bbb4d645092bf8eada376bf6b222cc0da1a6556
Mindou <minsu5875@naver.com>
2023-05-26 07:45:08 +0900

    wip: realize GUI 2

--- include/main.h
+++ include/main.h
@@ -29,6 +29,14 @@
  *  
  */
 
+#define TARGET_FPS          60                               // target fps
+#define SCREEN_WIDTH        1024                             // screen size: width
+#define SCREEN_HEIGHT       768                              // screen size: height
+#define BACKGROUND_COLOR    ((Color) { 255, 255, 255, 255 }) // screen background basic color
+
+bool FILE_NOT_FOUND       = false;                           // file find flag
+int  btnColorTag[7];                                         // define color flag for each buttons
+
 /**
  * @brief print error and exit
  * 
--- src/main.c
+++ src/main.c
@@ -32,37 +32,30 @@
  *  
  */
 
-#define TARGET_FPS          60                               // target fps
-#define SCREEN_WIDTH        1024                             // screen size: width
-#define SCREEN_HEIGHT       768                              // screen size: height
-#define BACKGROUND_COLOR    ((Color) { 255, 255, 255, 255 }) // screen background basic color
-#define BUTTON_CORLOR       ((Color) {  30,  30,  30, 255 }) // button background basic color
-
-bool FILE_NOT_FOUND       = false;
-
-const Vector2 btnPos[] = {                         // define button display position
-    {.x = 30, .y =                       40},
-    {.x = 30, .y = SCREEN_HEIGHT * 0.1 + 40},
-    {.x = 30, .y = SCREEN_HEIGHT * 0.2 + 40},
-    {.x = 30, .y = SCREEN_HEIGHT * 0.3 + 40},
-    {.x = 30, .y = SCREEN_HEIGHT * 0.4 + 40},
-    {.x = 30, .y = SCREEN_HEIGHT * 0.5 + 40},
-    {.x = 30, .y = SCREEN_HEIGHT * 0.6 + 40}
+const Color btnColor[] = {                                  // button color flag
+    {  30,  30,  30, 255},
+    { 150, 150, 150, 255}
 };
 
-const char* algorithm[] = {
-    "FCFS",
-    "SJF",
-    "HRN",
-    "NPP",
-    "PP",
-    "RR",
-    "SRT"
+const Rectangle btnSize = { .width = 100, .height = 62 };   // button size
+
+const Rectangle btnPos[] = {                                // define button display position
+    { .x = 30, .y =                       40 , .width = 100, .height = 62 },
+    { .x = 30, .y = SCREEN_HEIGHT * 0.1 + 40 , .width = 100, .height = 62 },
+    { .x = 30, .y = SCREEN_HEIGHT * 0.2 + 40 , .width = 100, .height = 62 },
+    { .x = 30, .y = SCREEN_HEIGHT * 0.3 + 40 , .width = 100, .height = 62 },
+    { .x = 30, .y = SCREEN_HEIGHT * 0.4 + 40 , .width = 100, .height = 62 },
+    { .x = 30, .y = SCREEN_HEIGHT * 0.5 + 40 , .width = 100, .height = 62 },
+    { .x = 30, .y = SCREEN_HEIGHT * 0.6 + 40 , .width = 100, .height = 62 }
+};
+
+const char* algorithm[] = {                                 // define script for algorithm name
+    "FCFS", "SJF", "HRN", "NPP", "PP", "RR", "SRT"
 };
 
 int main(void) {    
 
-    // search and load "data.txt" files with process data
+    // search and load `data.txt` files with process data
 
     FILE *fp = fopen("data.txt", "r");
     if(fp == NULL) {
@@ -120,12 +113,11 @@ int main(void) {
     SRT(p, count, total); */
 
     // default config settings
-    //SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "CPU Scheduling Simulator with raylib");
     SetTargetFPS(TARGET_FPS);
 
     // loading and creating texture image from `res` folder
-    // WARN: be sure to call `InitWindows()` before creating a texture image 
+    // WARN: be sure to call `InitWindows()` before creating a texture image
 
     Texture2D logo_6pm_256x256      = LoadTexture("res/images/6pm-logo_256x256.png");
     Texture2D logo_raylib_128x128   = LoadTexture("res/images/raylib-logo_128x128.png");
@@ -134,7 +126,6 @@ int main(void) {
     // define a rectangle that represents the boundary of the screen
 
     const Rectangle bounds = { .width = SCREEN_WIDTH, .height = SCREEN_HEIGHT };
-    const Rectangle button = { .width = 100, .height = 62 };
 
     // define position for drawing logo
 
@@ -148,8 +139,15 @@ int main(void) {
         0.95f * (SCREEN_HEIGHT - logo_raylib_128x128.height)
     };
 
-    if(FILE_NOT_FOUND == true) {    // if program can't find the file
-        while (!WindowShouldClose()) {  // repeat until the user closes the window or presses the `ESC` key
+    // define position for default mouse position
+    
+    Vector2 mousePoint = { 0.0f, 0.0f };
+
+    // if program can't find the file
+    if(FILE_NOT_FOUND == true) {
+
+    // repeat until the user closes the window or presses the `ESC` key
+        while (!WindowShouldClose()) {  
 
             // initalizing frame buffer for drawing
             BeginDrawing();
@@ -157,7 +155,7 @@ int main(void) {
             // fill frame buffer to black
             ClearBackground(BLACK);
             
-            // draw dark black of rectangle on the screen 
+            // draw `BACKGROUND_COLOR` of rectangle on the screen 
             DrawRectangleRec(bounds, BACKGROUND_COLOR);
             
             // draw text on the screen
@@ -170,8 +168,30 @@ int main(void) {
             // draw screen using doulbe buffer method, ready to next frame buffer
             EndDrawing();
         }
-    } else {    // if program find the file
-        while (!WindowShouldClose()) { // repeat until the user closes the window or presses the `ESC` key
+    } 
+
+    // if program find the file
+    else {
+
+        // repeat until the user closes the window or presses the `ESC` key
+        while (!WindowShouldClose()) {
+
+            // mouse activate sensing
+            mousePoint = GetMousePosition();
+
+            // check mouse state
+            for (int i = 0; i < 7; i++) {
+                if (CheckCollisionPointRec(mousePoint, btnPos[i])) {
+
+                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                        btnColorTag[i] = 1;
+                    }
+                    else btnColorTag[i] = 1;
+
+                    if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnColorTag[i] = 0;
+                }
+                else btnColorTag[i] = 0;
+            }
 
             // initalizing frame buffer for drawing
             BeginDrawing();
@@ -179,16 +199,17 @@ int main(void) {
             // fill frame buffer to black
             ClearBackground(BLACK);
 
-            // draw dark black of rectangle on the screen 
+            // draw `BACKGROUND_COLOR` of rectangle on the screen 
             DrawRectangleRec(bounds, BACKGROUND_COLOR);
 
             // draw button on the screen
             for(int i = 0; i < 7; i++) {
-                DrawRectangle(btnPos[i].x, btnPos[i].y, button.width, button.height, BUTTON_CORLOR);
-                DrawText( algorithm[i], btnPos[i].x + 10, btnPos[i].y + 15, 30.0f, WHITE);
+
+                DrawRectangleRec( btnPos[i], btnColor[btnColorTag[i]] );
+                DrawText( algorithm[i], btnPos[i].x + 10, btnPos[i].y + 15, 30.0f, WHITE );
             }
 
-            // draw the texture at the specified position on the screen
+            // draw the texture at the `logo_%name%_position` on the screen
             DrawTextureV(   logo_6pm_256x256,    logo_6pm_position, WHITE);
             DrawTextureV(logo_raylib_128x128, logo_raylib_position, WHITE);
 
@@ -199,7 +220,6 @@ int main(void) {
             EndDrawing();
         }
     }
-
 
     // memory allocate disable
     UnloadTexture(logo_6pm_256x256);
